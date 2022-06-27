cac7a5b28a0922f43082022aa4e15571d8fd2711
Gigi <gigi@iMacs-iMac.local>
2022-06-27 18:29:18 -0500

    Almost there

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,59 +1,52 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { JWT_SECRET } = require("../secrets/index");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {
+  checkUsernamePassword,
+  checkNameTaken,
+  checkUserExists,
+} = require("./auth-middleware");
+const Users = require("./auth-model");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+router.post(
+  "/register",
+  checkUsernamePassword,
+  checkNameTaken,
+  (req, res, next) => {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+    Users.add({ username, password: hash })
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch(next);
+  }
+);
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
+router.post(
+  "/login",
+  checkUsernamePassword,
+  checkUserExists,
+  (req, res, next) => {
+    bcrypt.compareSync(req.body.password, req.user.password)
+      ? res.status(200).json({
+          message: `welcome, ${req.user.username}`,
+          token: buildToken(req.user),
+        })
+      : next({ status: 401, message: "invalid credentials" });
+  }
+);
 
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
-});
-
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
-});
+function buildToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,120 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const server = require("./server");
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const bcrypt = require("bcryptjs");
+const jwtDecode = require("jwt-decode");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("users").truncate();
+  await db("users").insert([
+    { username: "foo", password: "12345" },
+    { username: "bar", password: "12345" },
+  ]);
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+test("[0] sanity check", () => {
+  expect(true).toBe(true);
+});
+
+describe("auth-router.js", () => {
+  describe("POST /api/auth/register", () => {
+    test("[1] - creates a new user", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "baz", password: "12345" });
+      const baz = await db("users").where("username", "baz").first();
+      expect(baz).toMatchObject({ username: "baz" });
+    });
+
+    test("[2] - saves the user with a bcrypted password instead of plain text", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "baz", password: "12345" });
+      const baz = await db("users").where("username", "baz").first();
+      expect(bcrypt.compareSync("12345", baz.password)).toBeTruthy();
+    });
+
+    test("[3] - responds with the correct error message when no username or password", async () => {
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send({ username: "", password: "" });
+      expect(res.body.message).toMatch(/username and password required/i);
+    });
+  });
+
+  describe("POST /api/auth/login", () => {
+    test("[4] - responds with the correct message on valid credentials", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "baz", password: "12345" });
+
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "baz", password: "12345" });
+
+      expect(res.body.message).toMatch(/welcome, baz/i);
+    });
+
+    test("[5] - responds with a token", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "baz", password: "12345" });
+
+      let res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "baz", password: "12345" });
+      let decoded = jwtDecode(res.body.token);
+      expect(decoded).toMatchObject({
+        subject: 3,
+        username: "baz",
+      });
+    });
+  });
+});
+
+describe("jokes-router.js", () => {
+  describe("GET /api/jokes", () => {
+    test("[6] / - requests without a token can not log in", async () => {
+      const res = await request(server).get("/api/jokes");
+      expect(res.body.message).toMatch(/token required/i);
+    });
+
+    test("[7] - requests with a token can view jokes", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "baz", password: "12345" });
+
+      let res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "baz", password: "12345" });
+
+      res = await request(server)
+        .get("/api/jokes")
+        .set("Authorization", res.body.token);
+
+      expect(res.body).toMatchObject([
+        {
+          id: "0189hNRf2g",
+          joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.",
+        },
+        {
+          id: "08EQZ8EQukb",
+          joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",
+        },
+        {
+          id: "08xHQCdx5Ed",
+          joke: "Why didn’t the skeleton cross the road? Because he had no guts.",
+        },
+      ]);
+    });
+  });
+});
